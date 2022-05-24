8bd6108efeb65169c98a87f7d713fc87fe0db282
Sihkdragon <ditolord@gmail.com>
2022-05-24 17:58:35 +0700

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html>
+      <Head />
+      <body className="bg-cyan-900 py-8">
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
